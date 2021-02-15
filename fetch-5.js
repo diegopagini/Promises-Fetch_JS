9c3d197b7fa4ebf28d66c08827cd27cbc0a66dcd
Diego Pagini <dq@qbkconsulting.com>
2021-02-15 07:15:39 -0300

    catch error

--- fetch-5.js
+++ fetch-5.js
@@ -1,14 +1,15 @@
 /** @format */
 
-fetch('https://reqres.in/api/users/1').then((resp) => {
-	resp
-		.clone()
-		.json()
-		.then((usuario) => {
-			console.log(usuario.data);
-		});
-
-	resp.json().then((usuario) => {
-		console.log(usuario.data);
+fetch('https://reqres.in/api/users/1000') // Los errores 400 no van al catch
+	.then((resp) => {
+		if (resp.ok) {
+			return resp.json();
+		} else {
+			throw new Error('No eiste el usuario 1000');
+		}
+	})
+	.then(console.log)
+	.catch((error) => {
+		console.log('Error en la peticion');
+		console.log(error);
 	});
-});
