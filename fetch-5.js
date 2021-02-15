f6f3ee90832373fbb33e268e95be018ee37fd78c
Diego Pagini <dq@qbkconsulting.com>
2021-02-15 07:01:06 -0300

    Response.clone()

---
+++ fetch-5.js
@@ -0,0 +1,14 @@
+/** @format */
+
+fetch('https://reqres.in/api/users/1').then((resp) => {
+	resp
+		.clone()
+		.json()
+		.then((usuario) => {
+			console.log(usuario.data);
+		});
+
+	resp.json().then((usuario) => {
+		console.log(usuario.data);
+	});
+});
